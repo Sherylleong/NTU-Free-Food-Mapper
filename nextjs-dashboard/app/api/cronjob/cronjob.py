c14911c264690dde028e305e70167143f7d1ce91
Leong Kit Ye <sherylleongkitye@gmail.com>
2024-12-11 11:20:35 +0800

    fix cronjob

--- nextjs-dashboard/app/api/cronjob/cronjob.py
+++ nextjs-dashboard/app/api/cronjob/cronjob.py
@@ -3,10 +3,10 @@ from os.path import join
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        with open(join('app','api', 'cronjob', 'freefood_scraper_azuredb.py'), 'r') as file:
+            code = file.read()
+            exec(code)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        with open(join('scraper_code', 'freefood_scraper_azuredb.py'), 'r') as file:
-            code = file.read()
-            exec(code)
         return
