e2532b2aef7b597fce78ae84b986619dad360beb
Leong Kit Ye <sherylleongkitye@gmail.com>
2024-12-17 11:51:24 +0800

    changed url to github

--- nextjs-dashboard/app/components/navbar.tsx
+++ nextjs-dashboard/app/components/navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
           <a href="https://t.me/s/freefoodntu" target="_blank" className ="flex items-center text-sm px-3 py-2 leading-none border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white ">NTU Free Food Telegram</a>
         </div>
         <div>
-          <a href="https://github.com/Sherylleong/NTUFreeFood-StatTracker" target="_blank" className ="flex items-center text-sm px-3 py-2 leading-none border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white ">View Code on Github</a>
+          <a href="https://github.com/Sherylleong/NTU-Free-Food-Mapper" target="_blank" className ="flex items-center text-sm px-3 py-2 leading-none border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white ">View Code on Github</a>
         </div>
 
     </div>
