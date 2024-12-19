99fcece4fa4271c63691b7c32725fb571d38f835
Leong Kit Ye <sherylleongkitye@gmail.com>
2024-12-19 10:00:57 +0800

    fixed last updated date

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -71,11 +71,11 @@ const Home = () => {
         const updateTime = await fetchLastUpdateTime();
         let date = new Date(updateTime);
         console.log(date, updateTime)
-        const year = date.getFullYear();
-        const month = String(date.getMonth()+1).padStart(2, '0'); // months are zero-based
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const year = date.getUTCFullYear();
+        const month = String(date.getUTCMonth()+1).padStart(2, '0'); // months are zero-based
+        const day = String(date.getUTCDate()).padStart(2, '0');
+        const hours = String(date.getUTCHours()).padStart(2, '0');
+        const minutes = String(date.getUTCMinutes()).padStart(2, '0');
         const dateString = `${year}-${month}-${day} ${hours}:${minutes}`;
         setLastUpdateTime(dateString); 
       } catch (error) {
